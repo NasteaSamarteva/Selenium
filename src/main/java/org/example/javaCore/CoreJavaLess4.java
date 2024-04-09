012b5896520017307aec03165ab6565e9843a894
Anastasia HAVERIUC <samartevan28@gmail.com>
2024-04-09 23:17:47 +0300

    Added forth JavaCore lesson

---
+++ src/main/java/org/example/javaCore/CoreJavaLess4.java
@@ -0,0 +1,13 @@
+package org.example.javaCore;
+
+public class CoreJavaLess4 {
+    public static void main(String[] args) {
+        CoreJavaLess4 variable1 = new CoreJavaLess4();
+        System.out.println(variable1.getData(4, 5));
+    }
+
+    public int getData(int a, int b) {
+        return a + b;
+    }
+
+}
