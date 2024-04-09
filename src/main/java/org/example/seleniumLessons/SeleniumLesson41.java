c5808b938d7e95e8271783d922125ed2b7058ba5
Anastasia HAVERIUC <samartevan28@gmail.com>
2024-04-09 23:18:41 +0300

    Added Selenium lesson 41

---
+++ src/main/java/org/example/seleniumLessons/SeleniumLesson41.java
@@ -0,0 +1,43 @@
+package org.example.seleniumLessons;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class SeleniumLesson41 {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("chromedriver.exe", "C:\\Users\\Nastya\\chromeDriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        String name = "rahul";
+        String password = getPassword(driver);
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.id("inputUsername")).sendKeys(name);
+        driver.findElement(By.name("inputPassword")).sendKeys(password);
+        driver.findElement(By.className("signInBtn")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        Thread.sleep(1000);
+
+
+        Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(), "Hello " + name + ",");
+        driver.findElement(By.xpath("//button[text()='Log Out']")).click();
+        driver.close();
+
+    }
+
+
+    public static String getPassword(WebDriver driver) throws InterruptedException {
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+        String passwordText = driver.findElement(By.cssSelector("form p")).getText();
+        String[] passArray = passwordText.split("'");
+        String password = passArray[1].split("'")[0];
+        return password;
+    }
+}
