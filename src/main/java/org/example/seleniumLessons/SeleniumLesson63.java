26fded0399c1eeb96134471303b2d18c6ef44ead
Anastasia HAVERIUC <samartevan28@gmail.com>
2024-04-09 23:21:45 +0300

    Added Selenium lesson 63

---
+++ src/main/java/org/example/seleniumLessons/SeleniumLesson63.java
@@ -0,0 +1,24 @@
+package org.example.seleniumLessons;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class SeleniumLesson63 {
+    public static void main(String[] args) throws InterruptedException {
+        String name = "Anastasia";
+        System.setProperty("chromedriver.exe", "C:\\Users\\Nastya\\chromeDriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.findElement(By.name("enter-name")).sendKeys(name);
+        driver.findElement(By.id("alertbtn")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().accept();
+        driver.switchTo().alert().dismiss();
+
+    }
+}
